4c5efece1355118ba57304831b41f27d9a05b4f4
aaakkk12 <112693569+aaakkk12@users.noreply.github.com>
2022-11-08 13:44:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ let songs = [
     {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Rogers & Dean - Bloodpressure", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Raptures. & MAZAN - Sweet", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Jay Eskar - To The Top", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "WBN & Mojnz - Radio", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "The Best of Egzod Mix ", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
